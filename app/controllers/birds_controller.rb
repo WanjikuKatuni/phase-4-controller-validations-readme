21329ec0f3ecb853a5b0299dae923b732e706b4a
wanjiku <annritawanjiku77@gmail.com>
2022-11-29 17:04:24 +0300

    complete

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -9,8 +9,19 @@ class BirdsController < ApplicationController
 
   # POST /birds
   def create
-    bird = Bird.create(bird_params)
+    # bird = Bird.create(bird_params)
+    # if bird.valid?
+    #   render json: bird, status: :created
+    # else
+    #   render json: {errors: bird.errors.full_messages}, status: :unprocessable_entity
+    # end
+
+    # or
+    bird = Bird.create!(bird_params)
     render json: bird, status: :created
+
+  rescue ActiveRecord::RecordInvalid => invalid 
+    render json: {errors: invalid.record.errors.full_messages}, status: :unprocessable_entity
   end
 
   # GET /birds/:id
@@ -22,8 +33,14 @@ class BirdsController < ApplicationController
   # PATCH /birds/:id
   def update
     bird = find_bird
-    bird.update(bird_params)
+    bird.update!(bird_params)
     render json: bird
+  rescue ActiveRecord::RecordInvalid => invalid 
+    render json: {errors: invalid.record.errors.full_messages}, status: :unprocessable_entity
+  end
+
+
+    
   end
 
   # DELETE /birds/:id
